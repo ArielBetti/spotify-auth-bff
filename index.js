b98a433b9400dda34bbd640ae23497d09aea0f4f
ArielBetti <ariel.betti@gmail.com>
2022-12-04 02:29:25 -0300

    build: remove html render

--- index.js
+++ index.js
@@ -10,15 +10,6 @@ app.use(cors());
 app.use(bodyParser.json()).use(cookieParser());
 require("./routes/authorization")(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
